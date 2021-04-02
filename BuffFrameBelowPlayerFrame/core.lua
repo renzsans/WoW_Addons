abb4219c6b1d961e40822713d432e5e9797e6246
Renz Santos <renzsans@gmail.com>
2021-04-02 10:38:11 -0500

    Add player class condition

--- BuffFrameBelowPlayerFrame/core.lua
+++ BuffFrameBelowPlayerFrame/core.lua
@@ -3,9 +3,23 @@ f:SetMovable(1)
 f:SetUserPlaced(true)
 f:SetScale(.85)
 
-hooksecurefunc("BuffFrame_UpdateAllBuffAnchors", function()
-  BuffButton1:ClearAllPoints()
-  BuffButton1:SetPoint("TOPLEFT", PlayerFrame, "BOTTOMRIGHT", -35, 10)  
-  DebuffButton1:ClearAllPoints()
-  DebuffButton1:SetPoint("BOTTOM", PlayerFrame, "BOTTOMRIGHT", -20, -120)
+local playerClass, englishClass = UnitClass("player");
+
+hooksecurefunc("BuffFrame_UpdateAllBuffAnchors", function()	
+  if englishClass == "DRUID" then
+    BuffButton1:ClearAllPoints()
+    BuffButton1:SetPoint("TOPLEFT", PlayerFrame, "BOTTOMRIGHT", -35, 10)  
+	DebuffButton1:ClearAllPoints()
+	DebuffButton1:SetPoint("BOTTOM", PlayerFrame, "BOTTOMRIGHT", -20, -120)
+  elseif englishClass == "HUNTER" then
+    BuffButton1:ClearAllPoints()
+    BuffButton1:SetPoint("TOPLEFT", PlayerFrame, "BOTTOMRIGHT", -35, -15)
+	DebuffButton1:ClearAllPoints()
+    DebuffButton1:SetPoint("BOTTOM", PlayerFrame, "BOTTOMRIGHT", -20, -125)
+  else
+    BuffButton1:ClearAllPoints()
+    BuffButton1:SetPoint("TOPLEFT", PlayerFrame, "BOTTOMRIGHT", -35, 10)
+	DebuffButton1:ClearAllPoints()
+	DebuffButton1:SetPoint("BOTTOM", PlayerFrame, "BOTTOMRIGHT", -20, -120)
+  end
 end)

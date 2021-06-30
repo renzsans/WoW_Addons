a91dd705e8420f1a13228329440db2784bbba792
Renz Santos <renzsans@gmail.com>
2021-06-30 11:40:45 -0500

    Update buff frame. Add gun sounds.

--- BuffFrameBelowPlayerFrame/BuffFrameBelowPlayerFrame.lua
+++ BuffFrameBelowPlayerFrame/BuffFrameBelowPlayerFrame.lua
@@ -1,20 +1,20 @@
 -- Set Player Frame left of the player character
 PlayerFrame:ClearAllPoints()
-PlayerFrame:SetPoint("CENTER", -220, -50)
+PlayerFrame:SetPoint("CENTER", -230, -20)
 -- PlayerFrame:SetPoint("BOTTOM", UIParent, "CENTER", -600, 600)
 PlayerFrame:SetUserPlaced(true);
 
 -- Set Target Frame right of the player character
 TargetFrame:ClearAllPoints()
-TargetFrame:SetPoint("CENTER", 220, -50)
+TargetFrame:SetPoint("CENTER", 230, -20)
 -- TargetFrame:SetPoint("BOTTOM", UIParent, "CENTER", -800, 500)
 TargetFrame:SetUserPlaced(true);
 
 -- Modify buff frame
-f=BuffFrame
-f:SetMovable(1)
-f:SetUserPlaced(true)
-f:SetScale(.85)
+-- f=BuffFrame
+-- f:SetMovable(1)
+-- f:SetUserPlaced(true)
+-- f:SetScale(.85)
 
 local playerClass, englishClass = UnitClass("player");
 
@@ -22,6 +22,8 @@ local playerClass, englishClass = UnitClass("player");
 hooksecurefunc("BuffFrame_UpdateAllBuffAnchors", function()
   BuffFrame:ClearAllPoints()
   BuffFrame:SetPoint("TOPRIGHT", PlayerFrame, "BOTTOMRIGHT", -5, -20)
+  BuffFrame:SetScale(.85)
+  BuffFrame:SetUserPlaced(true)
   -- if englishClass == "HUNTER" then
   --   BuffButton1:ClearAllPoints()
   --   BuffButton1:SetPoint("TOPLEFT", PlayerFrame, "BOTTOMRIGHT", -35, -20)
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # WoW_Addons
-Custom addon folder for World of Warcraft
+Custom Addons for World of Warcraft TBC Classic
 
-### Buff Frame Below Player Frame
+## Buff Frame Below Player Frame
 
-<img src="images/sample.png" alt="sample" width="500"/>
+<img src="BuffFrameBelowPlayerFrame/images/sample.png" alt="sample" width="500"/>
+
+---
+
+## Gun Sounds
+
+Copy paste gun sounds `Sound` folder to C:\Program Files (x86)\World of Warcraft\_classic_

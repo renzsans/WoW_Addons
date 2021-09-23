696875aac0a1fb4bda1b2fe1faa8f758eb253303
Renz Santos <renzsans@gmail.com>
2021-09-23 07:38:38 -0500

    Updates

--- BuffFrameBelowPlayerFrame/BuffFrameBelowPlayerFrame.lua
+++ BuffFrameBelowPlayerFrame/BuffFrameBelowPlayerFrame.lua
@@ -1,14 +1,14 @@
 -- Set Player Frame left of the player character
 PlayerFrame:ClearAllPoints()
-PlayerFrame:SetPoint("CENTER", -230, -20)
+PlayerFrame:SetPoint("CENTER", -230, 10)
 -- PlayerFrame:SetPoint("BOTTOM", UIParent, "CENTER", -600, 600)
-PlayerFrame:SetUserPlaced(true);
+PlayerFrame:SetUserPlaced(true)
 
 -- Set Target Frame right of the player character
 TargetFrame:ClearAllPoints()
-TargetFrame:SetPoint("CENTER", 230, -20)
+TargetFrame:SetPoint("CENTER", 230, 10)
 -- TargetFrame:SetPoint("BOTTOM", UIParent, "CENTER", -800, 500)
-TargetFrame:SetUserPlaced(true);
+TargetFrame:SetUserPlaced(true)
 
 -- Modify buff frame
 -- f=BuffFrame
